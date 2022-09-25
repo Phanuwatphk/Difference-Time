41edc8f22d379ba4e7ff87d98c8d60f2af6c3804
Phanuwatphk <flook5067@gmail.com>
2022-09-25 23:44:39 +0700

    another feature

--- Difference Time.py
+++ Difference Time.py
@@ -1,4 +1,4 @@
-from datetime import *
+from datetime import datetime, timedelta
 import math
 
 monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]#จำนวนวันในแต่ละเดือน
@@ -27,21 +27,21 @@ def getDifference(dt1, dt2):#หาความต่างระหว่าง
 
     return int(math.fabs(n2 - n1))#return ความห่างระหว่างวัน
 
-def cal_f1():#ความต่างวันที่
-    print("1 = เริ่มจากวันนี้\n2 = กรอกวันที่เริ่มต้นเอง")#(กรอกเป็นค.ศ. และต้องเป็นวันหลังจากวันนี้)
+def cal_f1():
+    print("1 = เริ่มจากวันนี้\n2 = กำหนดวันที่เอง")
     A = input("ตัวเลือก : ")
 
     if A == '1':#เริ่มจากวันนี้
         td = datetime.now()#return เวลาปัจจุบัน
-        dt = input("ใส่วันที่ปลายทาง(วัน/เดือน/ปี) : ").split('/')#เก็บข้อมูลโดยแยกระหว่างวัน เดือน ปีด้วย "/"
+        dt = input("ใส่วันที่ที่จะเทียบ (วัน/เดือน/ปี) : ").split('/')#เก็บข้อมูลโดยแยกระหว่างวัน เดือน ปีด้วย "/"
         d, m, y = [int(item) for item in dt]#กรอกวัน เดือน ปีจาก dt ใส่ d, m, y
         d1 = Date(td.day, td.month, td.year)#กำหนดค่าวันที่ของปัจจุบันใส่ออบเจ็ค Date เพื่อคำนวณ
         d2 = Date(d, m, y)#กำหนดค่าวันที่ ที่รับเข้ามาใส่ออบเจ็ค Date เพื่อคำนวณ
-    elif A == '2':#กรอกวันที่เริ่มต้นเอง
-        d1 = input("ใส่วันที่ต้นทาง(วัน/เดือน/ปี) : ").split('/')
+    elif A == '2':#กำหนดวันที่เอง
+        d1 = input("ใส่วันที่ที่ 1 (วัน/เดือน/ปี) : ").split('/')
         d, m, y = [int(item) for item in d1]
         d1 = Date(d, m, y)
-        d2 = input("ใส่วันที่ปลายทาง(วัน/เดือน/ปี) : ").split('/')
+        d2 = input("ใส่วันที่ที่ 2 (วัน/เดือน/ปี) : ").split('/')
         d, m, y = [int(item) for item in d2]
         d2 = Date(d, m, y)
     sum = getDifference(d1, d2)#หาความต่างวันที่
@@ -59,44 +59,55 @@ def cal_f1():#ความต่างวันที่
         if d1.m + i > 11:#ถ้าเดือนที่คิดเกินธันวาคม
           d1.m = 0#เดือนที่หนึ่งในปีใหม่
           i = 0
-        if sum < monthDays[d1.m + i]:#วันที่ ที่เหลือหาจำนวนเดือนไม่ได้แล้ว
+        if sum < monthDays[d1.m + i]:#วันที่ที่เหลือหาจำนวนเดือนไม่ได้แล้ว
             break
         sum -= 30 + (monthDays[d1.m + i] - 30)#หาจำนวนเดือน
         m += 1
         i += 1
     print("ห่างกัน", y, "ปี", m, "เดือน", sum, "วัน (%d วัน)" % s)#แสดงผล
 
-def cal_f2():#ความต่างเวลา
-    print("1 = เริ่มจากตอนนี้\n2 = กรอกเวลาเริ่มต้นเอง")
+def cal_f2():
+    print("1 = เริ่มจากตอนนี้\n2 = กำเวลาเอง")
     A = input("ตัวเลือก : ")
 
     if A == '1':#เริ่มจากตอนนี้
         t1 = datetime.utcnow() + timedelta(hours = 7)#return เวลาปัจจุบันของประเทศไทย(UTC+7) เข้าออบเจ็ค datetime แล้วเก็บไว้ใน t1
-        t2 = datetime.strptime(input("ใส่เวลาปลายทาง(ชั่วโมง:นาที) : "), "%H:%M")#รับข้อมูลเวลาเข้าออบเจ็ค datetime แล้วเก็บไว้ใน t2
-    elif A == '2':#กรอกเวลาเริ่มต้นเอง
-        t1 = datetime.strptime(input("ใส่เวลาต้นทาง(ชั่วโมง:นาที) : "), "%H:%M")
-        t2 = datetime.strptime(input("ใส่เวลาปลายทาง(ชั่วโมง:นาที) : "), "%H:%M")
-    delta = t2 - timedelta(hours = t1.hour, minutes = t1.minute)#เอาเวลาปลายทาง(t2) - เวลาต้นทาง(t1) แล้วเก็บไว้ใน delta
+        t2 = datetime.strptime(input("ใส่เวลาที่จะเทียบ (ชั่วโมง:นาที) : "), "%H:%M")#รับข้อมูลเวลาเข้าออบเจ็ค datetime แล้วเก็บไว้ใน t2
+    elif A == '2':#กำหนดเวลาเอง
+        t1 = datetime.strptime(input("ใส่เวลาที่ 1 (ชั่วโมง:นาที) : "), "%H:%M")
+        t2 = datetime.strptime(input("ใส่เวลาที่ 2 (ชั่วโมง:นาที) : "), "%H:%M")
+    if t2 > t1:#เมื่อเวลาที่ 1 คืออดีตและเวลาที่ 2 คืออนาคต
+        delta = t2 - timedelta(hours = t1.hour, minutes = t1.minute)#เอาเวลาที่ 2 (t2) - เวลาที่ 1 (t1) แล้วเก็บไว้ใน delta
+    else:#เมื่อเวลาที่ 1 คืออนาคตและเวลาที่ 2 คืออดีต
+        delta = t1 - timedelta(hours = t2.hour, minutes = t2.minute)#เอาเวลาที่ 1 (t1) - เวลาที่ 2 (t2) แล้วเก็บไว้ใน delta
+
     print("ห่างกัน", delta.hour, "ชั่วโมง", delta.minute, "นาที")#แสดงผล
 
-def cal_f3():#ความต่างวันที่และเวลา
-    print("1 = เริ่มจากวันที่วันนี้และเวลาตอนนี้\n2 = กรอกวันที่และเวลาเริ่มต้นเอง")
+def cal_f3():
+    print("1 = เริ่มจากวันที่และเวลาตอนนี้\n2 = กำหนดวันที่และเวลาเอง")
     A = input("ตัวเลือก : ")
 
     if A == '1':#เริ่มจากตอนนี้
         d1 = datetime.utcnow() + timedelta(hours = 7)#return เวลาปัจจุบันของประเทศไทย(UTC+7) เข้าออบเจ็ค datetime แล้วเก็บไว้ใน t1
-        d2 = datetime.strptime(input("ใส่วันที่และเวลาปลายทาง(วัน/เดือน/ปี ชั่วโมง:นาที) : "), "%d/%m/%Y %H:%M")#นำเข้าข้อมูล วัน เดือน ปีและเวลา(วัน/เดือน/ปี ชั่วโมง:นาที)
+        d2 = datetime.strptime(input("ใส่วันที่และเวลาที่จะเทียบ (วัน/เดือน/ปี ชั่วโมง:นาที) : "), "%d/%m/%Y %H:%M")#นำเข้าข้อมูล วัน เดือน ปีและเวลา(วัน/เดือน/ปี ชั่วโมง:นาที)
         dt1 = Date(d1.day, d1.month, d1.year)#เก็บวันที่ปัจจุบันใส่ออบเจ็ค Date แล้วเก็บไว้ในตัวแปร dt1
         dt2 = Date(d2.day, d2.month, d2.year)#เก็บวันที่นำเข้าใส่ออบเจ็ค Date แล้วเก็บไว้ในตัวแปร dt2
-    elif A == '2':#กรอกเวลาเริ่มต้นเอง
-        d1 = datetime.strptime(input("ใส่วันที่และเวลาต้นทาง(วัน/เดือน/ปี ชั่วโมง:นาที) : "), "%d/%m/%Y %H:%M")
-        d2 = datetime.strptime(input("ใส่วันที่และเวลาปลายทาง(วัน/เดือน/ปี ชั่วโมง:นาที) : "), "%d/%m/%Y %H:%M")
+    elif A == '2':#กำหนดเวลาเอง
+        d1 = datetime.strptime(input("ใส่วันที่และเวลาที่ 1 (วัน/เดือน/ปี ชั่วโมง:นาที) : "), "%d/%m/%Y %H:%M")
+        d2 = datetime.strptime(input("ใส่วันที่และเวลาที่ 2 (วัน/เดือน/ปี ชั่วโมง:นาที) : "), "%d/%m/%Y %H:%M")
         dt1 = Date(d1.day, d1.month, d1.year)
         dt2 = Date(d2.day, d2.month, d2.year)
     sum = getDifference(dt1, dt2)#หาความต่างวันที่
-    if (d1.hour > d2.hour) or (d1.hour == d2.hour and d1.minute > d2.minute):#ตรวจหาวันว่าเวลามันข้ามไปอีกวันมั้ย
-        sum -= 1
-    tsum = d2 - timedelta(hours = d1.hour, minutes = d1.minute)#เอาเวลาปลายทาง(จาก d2) - เวลาต้นทาง(จาก d1) แล้วเก็บไว้ในtsum
+    if d2 > d1:#เมื่อเวลาที่ 1 คืออดีตและเวลาที่ 2 คืออนาคต
+        tsum = d2 - timedelta(hours = d1.hour, minutes = d1.minute)#เอาเวลาปลายทาง(จาก d2) - เวลาต้นทาง(จาก d1) แล้วเก็บไว้ในtsum
+        if (d1.hour > d2.hour) or (d1.hour == d2.hour and d1.minute > d2.minute):#ตรวจหาวันว่าเวลามันข้ามไปอีกวันมั้ย
+          sum -= 1
+          tsum += timedelta(hours = 24)
+    else:#เมื่อเวลาที่ 1 คืออนาคตและเวลาที่ 2 คืออดีต
+        tsum = d1 - timedelta(hours = d2.hour, minutes = d2.minute)#เอาเวลาปลายทาง(จาก d2) - เวลาต้นทาง(จาก d1) แล้วเก็บไว้ในtsum
+        if (d2.hour > d1.hour) or (d2.hour == d1.hour and d2.minute > d1.minute):#ตรวจหาวันว่าเวลามันข้ามไปอีกวันมั้ย
+          sum -= 1
+          tsum += timedelta(hours = 24)
     m = 0
     s = sum#ระยะห่างทั้งหมด(วัน)
     y = sum // 365#หาจำนวนปีที่ห่าง
@@ -118,9 +129,9 @@ def cal_f3():#ความต่างวันที่และเวลา
         i += 1
     print("ห่างกัน", y, "ปี", m, "เดือน", sum, "วัน (%d วัน)" % s, tsum.hour, "ชั่วโมง", tsum.minute, "นาที")#แสดงผล
 
-print("Difference Time")  #ชื่อโปรแกรม
+print("Difference Time")#ชื่อโปรแกรม
 
-while True:#main function
+while True:
     print("=" * 50 + "\n1 = คำนวณหาความต่างวันที่")
     print("2 = คำนวณหาความต่างเวลา")
     print("3 = คำนวณหาความต่างวันที่และเวลา")
